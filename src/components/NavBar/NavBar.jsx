4ac3aa38a59979f591d8c60567b80fbb9f874e5b
me-mulet <mercedes.mulet@gmail.com>
2021-10-08 16:16:49 -0500

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+    return(
+        <>
+        <nav className='nav'>
+        <div className='container-title'>
+            <h1>CORTADORES RAISE</h1>
+        </div>
+        <ul className='nav-ul'>
+            <li className='nav-li'><a href="/#" className='a'>Inicio</a></li>
+            <li className='nav-li'><a href="/#" className='a'>Tienda</a></li>
+            <li className='nav-li'><a href="/#" className='a'>Cómo comprar</a></li>
+            <li className='nav-li'><a href="/#" className='a'>Contacto</a></li>
+        </ul>
+    </nav>
+    </>
+    )
+}
