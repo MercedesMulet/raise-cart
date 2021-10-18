13a7b57b9714b2ceb52db1f4926d5c2e51976033
me-mulet <mercedes.mulet@gmail.com>
2021-10-17 19:26:27 -0500

    detalle carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export const CartWidget = () => {
-    return(
-        <div className="cart-nav-icon">
-            <FontAwesomeIcon icon={faShoppingCart}/>        
-        </div>
-    );
-}
+  return (
+    <div className="cart-nav-icon">
+      <FontAwesomeIcon icon={faShoppingCart} />
+      <span className="count-cart-widget">1</span>
+    </div>
+  );
+};
