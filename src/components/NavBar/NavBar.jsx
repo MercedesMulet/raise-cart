c7fc13bdb648d33151a06293184f41552eff9f89
me-mulet <mercedes.mulet@gmail.com>
2021-10-17 19:40:30 -0500

    Logo añadido

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,37 @@
 import { CartWidget } from '../CartWidget/CartWidget';
 
 export const NavBar = () => {
-    return(
-        <>
-        <nav className='nav'>
-        <div className='container-title'>
-            <h2>CORTADORES RAISE</h2>
+  return (
+    <>
+      <nav className="nav">
+        <div className="container-logo">
+          <img src="images/LogoRaiseVerde.png" alt="Logo Raise" />
         </div>
-        <ul className='nav-ul'>
-            <li className='nav-li'><a href="/#" className='a'>Inicio</a></li>
-            <li className='nav-li'><a href="/#" className='a'>Tienda</a></li>
-            <li className='nav-li'><a href="/#" className='a'>Cómo comprar</a></li>
-            <li className='nav-li'><a href="/#" className='a'>Contacto</a></li>
+        <ul className="nav-ul">
+          <li className="nav-li">
+            <a href="/#" className="a">
+              Inicio
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/#" className="a">
+              Tienda
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/#" className="a">
+              Cómo comprar
+            </a>
+          </li>
+          <li className="nav-li">
+            <a href="/#" className="a">
+              Contacto
+            </a>
+          </li>
 
-            <CartWidget />
+          <CartWidget />
         </ul>
-    </nav>
+      </nav>
     </>
-    )
-}
+  );
+};
