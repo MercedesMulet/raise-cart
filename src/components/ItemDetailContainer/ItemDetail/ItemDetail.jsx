2982ea443420f99941a60fc715bd999b71af0d25
me-mulet <mercedes.mulet@gmail.com>
2021-11-16 20:45:47 -0500

    firebase instalado

--- src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail/ItemDetail.jsx
@@ -1,39 +1,36 @@
 import { Button } from '../../Button/Button';
 import { ItemCount } from '../../ItemCount/ItemCount';
 import { getFirestore } from '@firebase/firestore';
-import { doc, getDoc } from '../../../firebase/index'
+import { doc, getDoc } from '../../../firebase/index';
 import { useEffect } from 'react';
 
 export const ItemDetail = ({ item }) => {
-
   return (
     <>
-      {
-        item.map(prod => {
-          return (
-            <div className="item-detail" key={prod.codigoRaise}> 
-              <div className="img-detail">
-                <img src={prod.img} alt="" />
-              </div>
-              <div className="content-detail">
-                <div className="info-detail">
-                  <h2 className="title-detail">
-                    {prod.categoria} {prod.referencia}
-                  </h2>
-                  <p>{prod.tipoLlaves}</p>
-                  <p>Para máquinas: {prod.modeloMaquina}</p>
-                  <p>Material {prod.material}</p>
-                  <span className="precio-detail">${prod.precio}</span>
-                </div>
-                <div className="cta-detail">
-                  <Button buttonStyle="btn-primary-solid">Comprar</Button>
-                </div>
-                 <ItemCount initial={0} stock={prod.stock}/> 
-              </div>
+      {item.map((prod) => {
+        return (
+          <div className="item-detail" key={prod.codigoRaise}>
+            <div className="img-detail">
+              <img src={prod.img} alt="" />
             </div>
-          );
-        })
-      }
+            <div className="content-detail">
+              <div className="info-detail">
+                <h2 className="title-detail">
+                  {prod.categoria} {prod.referencia}
+                </h2>
+                <p>{prod.tipoLlaves}</p>
+                <p>Para máquinas: {prod.modeloMaquina}</p>
+                <p>Material {prod.material}</p>
+                <span className="precio-detail">${prod.precio}</span>
+              </div>
+              <div className="cta-detail">
+                <Button buttonStyle="btn-primary-solid">Comprar</Button>
+              </div>
+              <ItemCount initial={0} stock={prod.stock} />
+            </div>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 };
