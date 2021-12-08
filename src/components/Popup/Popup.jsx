f26ed6658f2988f6433aefe4b8eac30043b57580
MercedesMulet <mercedes.mulet@gmail.com>
2021-12-08 12:15:06 -0500

    aplicando useHistory en el final del checkout

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,8 +1,17 @@
+import { useHistory } from "react-router";
+
 export const Popup = (props) => {
+  const history = useHistory()
+
+  const closePopUp = () => {
+    props.setTrigger(false);
+    history.push('/')
+  };
+
   return props.trigger ? (
     <div className="popup">
       <div className="popup-content">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>
+        <button className="close-btn" onClick={() => closePopUp()}>
           <strong>X</strong> Cerrar
         </button>
         {props.children}
